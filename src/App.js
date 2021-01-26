0ec63ab6fc068ce48e14e9598a474de1ede7a07c
melvincwng <melvincwng@users.noreply.github.com>
2021-01-26 17:39:52 +0800

    Update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from "react";
 import "./App.css";
 import pokemonData from "./pokemon/pokemon";
+import PokemonCard from "./PokemonCard.js";
+
+// functional component means -> to create a new js file that we can reuse
+// hence in this case, we need to create a functional component called PokemonCard
+// so create a new file called PokemonCard.js and write our functional component there
 
 const bulbasaur = pokemonData[0];
-
-function PokemonCard({ pokemon }) {
-  const { id, name, type, base } = pokemon;
-  return (
-    <div>
-      <div>{name.english}</div>
-    </div>
-  );
-}
+const ivysaur = pokemonData[1];
+const venusaur = pokemonData[2];
 
 function App() {
   return (
     <div className="App">
       <PokemonCard pokemon={bulbasaur} />
+      <PokemonCard pokemon={ivysaur} />
+      <PokemonCard pokemon = {venusaur} />
     </div>
   );
 }
 
 export default App;
+
