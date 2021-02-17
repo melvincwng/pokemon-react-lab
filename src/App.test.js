3cfcd1240c65fb202275e9ff532b90402f3a3d6d
melvincwng <melvincwng@users.noreply.github.com>
2021-02-17 18:47:03 +0800

    Update files for publishing again

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/* import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
@@ -6,4 +6,18 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+*/
+
+import { render } from "@testing-library/react";
+import App from "./App";
+import React from "react";
+import '@testing-library/jest-dom' //apparently if i I don't use this line, will have error that
+// toBeInDocument() does not work -> found solution online saying need to install & import jest-dom
+
+
+test("renders Pokemon Gallery component", () => {
+  const { getByText } = render(<App />);
+  const element = getByText(/Charmander/);
+  expect(element).toBeInTheDocument();
 });
