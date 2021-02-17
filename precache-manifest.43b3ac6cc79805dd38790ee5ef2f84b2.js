e6d58c9a4755cd39f582e5c9711a4a8833979b39
melvincwng <melvincwng@users.noreply.github.com>
2021-02-17 19:09:41 +0800

    Updates

---
+++ precache-manifest.43b3ac6cc79805dd38790ee5ef2f84b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2beca9f84c4239fb968272e424f2d86d",
+    "url": "/pokemon-react-lab/index.html"
+  },
+  {
+    "revision": "4b1055ede3d8df499b59",
+    "url": "/pokemon-react-lab/static/css/main.bc855b78.chunk.css"
+  },
+  {
+    "revision": "ec6736e3e5167b4fa6c1",
+    "url": "/pokemon-react-lab/static/js/2.e426d20d.chunk.js"
+  },
+  {
+    "revision": "4b1055ede3d8df499b59",
+    "url": "/pokemon-react-lab/static/js/main.26682940.chunk.js"
+  },
+  {
+    "revision": "db8cac7db41adb15d20a",
+    "url": "/pokemon-react-lab/static/js/runtime~main.45b1c067.js"
+  }
+]);
